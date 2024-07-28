ef03137edef2917464e996edaefdaf1805b7c804
Lasith Manujitha <Manujitha98@gmail.com>
2024-07-28 13:52:41 +0530

    Show title result

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ sys.path.append(os.path.join(parent_folder_path, "lib"))
 sys.path.append(os.path.join(parent_folder_path, "plugin"))
 
 from flowlauncher import FlowLauncher
-import webbrowser
-import requests
 from yt_dlp import YoutubeDL
 
 
@@ -21,9 +19,7 @@ class AnyVideo(FlowLauncher):
     def query(self, query):
         output = []
         if len(query.strip()) == 0:
-            output.append(
-                {"Title": "Enter a video URL", "IcoPath": "Images/app.png"}
-            )
+            output.append({"Title": "Enter a video URL", "IcoPath": "Images/app.png"})
 
         else:
             ydl_opts = {
@@ -39,26 +35,35 @@ class AnyVideo(FlowLauncher):
             except Exception as e:
                 output.append({"Title": f"Error: {e}", "IcoPath": "Images/app.png"})
                 return output
+            
+            output.append(
+                {
+                    "Title": info['title'],
+                    "SubTitle": "Select the format to download",
+                    "IcoPath": "Images/app.png",
+                }
+            )
 
             for format in info["formats"]:
-                if format["resolution"] is not None:
+                if format["resolution"] is not None and format["tbr"] is not None:
                     output.append(
                         {
                             "Title": f"Resolution: {format['resolution']}",
-                            "SubTitle": f"Bitrate: {format['tbr']} {format['video_ext']}",
+                            "SubTitle": f"Bitrate: {format['tbr']}",
                             "IcoPath": "Images/app.png",
                             "JsonRPCAction": {
                                 "method": "download",
-                                "parameters": [query, f"{format['format_id']}"]
+                                "parameters": [query, f"{format['format_id']}"],
                             },
                         }
                     )
 
         return output
-    
+
     def download(self, url, format_id):
         command = f"yt-dlp -f {format_id} {url} -P ~/Downloads -o %(title)s.%(ext)s --windows-filenames --no-overwrites --quiet --progress"
         os.system(command)
+
 
 if __name__ == "__main__":
     AnyVideo()
