a47112e65dfd1a4516ecb1b37a6b1b73de190be1
Lasith Manujitha <Manujitha98@gmail.com>
2024-08-08 02:59:34 +0530

    feat: Update yt-dlp.exe periodically

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ sys.path.append(os.path.join(parent_folder_path, "plugin"))
 from flowlauncher import FlowLauncher
 from yt_dlp import YoutubeDL
 
-DOWNLOAD_PATH = os.path.join(parent_folder_path, "yt-dlp.exe")
+EXE_PATH = os.path.join(parent_folder_path, "yt-dlp.exe")
 CHECK_INTERVAL_DAYS = 10
 
 
@@ -130,11 +130,14 @@ class AnyVideo(FlowLauncher):
 
     def download(self, url, format_id):
         # Check if yt-dlp.exe needs to be updated
-        last_modified_time = datetime.fromtimestamp(os.path.getmtime(DOWNLOAD_PATH))
+        last_modified_time = datetime.fromtimestamp(os.path.getmtime(EXE_PATH))
+        base_command = f"yt-dlp {url} -f {format_id}+ba -P ~/Downloads/AnyDownloader --windows-filenames --quiet --progress --no-mtime --force-overwrites --no-part"
+
+        # Check if the yt-dlp.exe needs to be updated
         if datetime.now() - last_modified_time >= timedelta(days=CHECK_INTERVAL_DAYS):
-            command = f"yt-dlp -f {format_id}+ba {url} -P ~/Downloads/AnyDownloader -U --windows-filenames --quiet --progress --no-mtime --force-overwrites --no-part"
+            command = f"{base_command} -U"
         else:
-            command = f"yt-dlp -f {format_id}+ba {url} -P ~/Downloads/AnyDownloader --windows-filenames --quiet --progress --no-mtime --force-overwrites --no-part"
+            command = base_command
 
         os.system(command)
 
