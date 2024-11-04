436bcf3e4819adb7cd240451841aee498ed4f35a
Lasith Manujitha <Manujitha98@gmail.com>
2024-11-05 01:47:24 +0530

    refactor: Improve query_result function formatting and readability

--- plugin/results.py
+++ plugin/results.py
@@ -25,13 +25,22 @@ def empty_result() -> Result:
     return Result(Title="Couldn't find any video formats.", IcoPath="Images/error.png")
 
 
-def query_result(query, thumbnail, title, format, download_path, pref_video_path, pref_audio_path) -> Result:
+def query_result(
+    query, thumbnail, title, format, download_path, pref_video_path, pref_audio_path
+) -> Result:
     return Result(
         Title=title,
-        SubTitle=f"{format['resolution']} ({round(format['tbr'])} kbps) {'┃ Size: ' + str(format['filesize']) if format.get('filesize') else ''} {'┃ FPS: ' + str(format['fps']) if format.get('fps') else ''}",
+        SubTitle=f"Res: {format['resolution']} ({round(format['tbr'], 2)} kbps) {'┃ Size: ' + str(round(format['filesize'] / 1024 / 1024, 2)) + 'MB' if format.get('filesize') else ''} {'┃ FPS: ' + str(int(format['fps'])) if format.get('fps') else ''}",
         IcoPath=thumbnail or "Images/app.png",
         JsonRPCAction={
             "method": "download",
-            "parameters": [query, f"{format['format_id']}", download_path, pref_video_path, pref_audio_path, format['resolution'] == 'audio only'],
+            "parameters": [
+                query,
+                f"{format['format_id']}",
+                download_path,
+                pref_video_path,
+                pref_audio_path,
+                format["resolution"] == "audio only",
+            ],
         },
     )
